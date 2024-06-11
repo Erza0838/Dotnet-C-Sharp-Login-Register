54fd32404914f29ec64ef65148dda473e283e038
Erza0838 <muhammaderza884@gmail.com>
2024-06-12 00:05:03 +0700

    Membuat Form register login bagian-2

---
+++ LoginRegister/LoginRegister/Controllers/CreateNewAccountController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LoginRegister.Controllers
+{
+    public class CreateNewAccountController : Controller
+    {
+        // GET: CreateNewAccountController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: CreateNewAccountController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: CreateNewAccountController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CreateNewAccountController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CreateNewAccountController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: CreateNewAccountController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CreateNewAccountController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: CreateNewAccountController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
